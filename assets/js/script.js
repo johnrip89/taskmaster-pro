1971107350f7d2d1af59d2f54ce219efc625e384
John Ripplinger <lebronjohn89@gmail.com>
2021-09-02 14:30:17 -0600

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,7 +123,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -198,15 +198,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
     console.log("activate", this);
   },
   deactivate: function (event) {
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
     console.log("deactivate", this);
   },
   over: function (event) {
+    $(event.target).addClass('dropover-active');
+    $('.bottom-trash').addClass('bottom-trash-active');
     console.log("over", event.target);
   },
   out: function (event) {
+    $(event.target).removeClass('dropover-active');
+    $('.bottom-trash').removeClass('bottom-trash-active');
     console.log("out", event.target);
   },
   update: function (event) {
@@ -263,4 +271,10 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
